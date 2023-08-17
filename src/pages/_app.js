0384e3553fa78eedd7b13e8b3385549ace39b8ed
Daniela Torres <dannytorres0211@gmail.com>
2023-08-17 17:03:10 -0500

    create project with next.js

---
+++ src/pages/_app.js
@@ -0,0 +1,15 @@
+import { ProviderAuth } from '@hooks/useAuth';
+import '../styles/tailwind.css';
+import MainLayout from '@layout/MainLayout';
+
+export default function App({ Component, pageProps }) {
+  return (
+    <>
+      <ProviderAuth>
+        <MainLayout>
+          <Component {...pageProps} />
+        </MainLayout>
+      </ProviderAuth>
+    </>
+  );
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,26 @@
 /** @type {import('tailwindcss').Config} */
+let colors = require('tailwindcss/colors');
+delete colors['lightBlue'];
+delete colors['warmGray'];
+delete colors['trueGray'];
+delete colors['coolGray'];
+delete colors['blueGray'];
+
+colors = {...colors, ...{transparent: 'transparent'}}
+
+
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        ...colors
       },
     },
   },
-  plugins: [],
-}
+  plugins: [require('@tailwindcss/forms')],
+};
